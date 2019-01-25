22cbd7bcb1d440de57b2d4cb6f25a0b244cc30d0
Radu Maier <ariademur@webserv.co>
2019-01-25 20:39:28 +0100

    Refactor exception

--- src/WebServCo/DiscogsApi/Exceptions/ApiException.php
+++ src/WebServCo/DiscogsApi/Exceptions/ApiException.php
@@ -1,9 +1,11 @@
 <?php
 namespace WebServCo\DiscogsApi\Exceptions;
 
-class DiscogsApiException extends \WebServCo\Framework\Exceptions\ApplicationException
+class ApiException extends \WebServCo\Framework\Exceptions\ApplicationException
 {
     const CODE = 0;
+
+    const DEFAULT_MESSAGE = 'Discogs Api Error';
 
     public function __construct($message, $code = self::CODE, \Exception $previous = null)
     {
