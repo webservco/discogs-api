ec3276d1c3f5fbc57bace99f00beed048f678f1d
Radu Maier <ariademur@webserv.co>
2019-02-19 12:22:44 +0100

    Utilities/Name

--- src/WebServCo/DiscogsApi/Parsers/Collection/Artists.php
+++ src/WebServCo/DiscogsApi/Parsers/Collection/Artists.php
@@ -5,11 +5,11 @@ final class Artists implements \WebServCo\DiscogsApi\Interfaces\ParserInterface
 {
     public static function parse($data)
     {
+        $nameUtility = new \WebServCo\DiscogsApi\Utilities\Name();
         $result = null;
         $items = count($data);
         for ($i = 0; $i < $items; $i++) {
-            $result .= !empty($data[$i]['anv']) ? $data[$i]['anv'] :
-            \WebServCo\DiscogsApi\Parsers\Name::parse($data[$i]['name']);
+            $result .= !empty($data[$i]['anv']) ? $data[$i]['anv'] : $nameUtility->removeNumbering($data[$i]['name']);
             $next = $i + 1;
             if (array_key_exists($next, $data)) {
                 $result .= " {$data[$i]['join']} ";
---
+++ src/WebServCo/DiscogsApi/Utilities/Name.php
@@ -0,0 +1,31 @@
+<?php
+namespace WebServCo\DiscogsApi\Utilities;
+
+final class Name
+{
+    protected $prefixes;
+
+    public function __construct($prefixes = [])
+    {
+        $this->prefixes = $prefixes ? $prefixes : [
+            'the',
+            'los',
+        ];
+    }
+
+    public function removeNumbering($data)
+    {
+        $result = preg_replace('/\([0-9]+\)/', '', (string) $data); // remove numbering
+        return trim((string) $result);
+    }
+
+    public function removePrefixes($data)
+    {
+        $patterns = [];
+        foreach ($this->prefixes as $item) {
+            $patterns[] = sprintf('/^%s /i', $item);
+        }
+
+        return preg_replace($patterns, '', $data);
+    }
+}
