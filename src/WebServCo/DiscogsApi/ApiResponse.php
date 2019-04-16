79e631e9e7f6a0f047afbec5415a3d4debe3a2ed
Radu Maier <ariademur@webserv.co>
2019-04-16 16:55:32 +0200

    ApiResponse extends \WebServCo\Api\AbstractResponse

--- src/WebServCo/DiscogsApi/ApiResponse.php
+++ src/WebServCo/DiscogsApi/ApiResponse.php
@@ -3,32 +3,13 @@ namespace WebServCo\DiscogsApi;
 
 use WebServCo\DiscogsApi\Exceptions\ApiResponseException;
 
-final class ApiResponse
+final class ApiResponse extends \WebServCo\Api\AbstractResponse
 {
     protected $endpoint;
     protected $data;
     protected $method;
     protected $response; // \WebServCo\Framework\Http\Response
     protected $status;
-
-    public function __construct($endpoint, $method, \WebServCo\Framework\Http\Response $response)
-    {
-        $this->endpoint = $endpoint;
-        $this->method = $method;
-        $this->response = $response;
-        $this->status = $this->response->getStatus();
-        $this->data = $this->processResponseData();
-    }
-
-    public function getData()
-    {
-        return $this->data;
-    }
-
-    public function getEndpoint()
-    {
-        return $this->endpoint;
-    }
 
     public function getErrorMessage()
     {
@@ -48,44 +29,8 @@ final class ApiResponse
         return ApiResponseException::DEFAULT_MESSAGE;
     }
 
-    public function getMethod()
-    {
-        return $this->method;
-    }
-
     public function getRateLimitRemaining()
     {
         return (int) $this->response->GetHeader('X-Discogs-Ratelimit-Remaining');
-    }
-
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    protected function processResponseData()
-    {
-        $responseContent = $this->response->getContent();
-        $contentType = $this->response->getHeader('Content-Type');
-        switch ($contentType) {
-            case 'application/json': // api
-                return json_decode($responseContent, true);
-                break;
-            case 'application/x-www-form-urlencoded': // oauth
-                if (false === strpos($responseContent, '=')) {
-                    /* Sometimes Discogs returns text/plain with this content type ... */
-                    return $responseContent;
-                }
-                $data = [];
-                parse_str($responseContent, $data);
-                return $data;
-                break;
-            case 'text/plain': // oauth
-                return $responseContent;
-                break;
-            default:
-                throw new ApiResponseException('Api returned unsupported content type.');
-                break;
-        }
     }
 }
