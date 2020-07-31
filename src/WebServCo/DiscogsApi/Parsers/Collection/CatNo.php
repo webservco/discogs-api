dc22f501777b7471772ab14a25a78fb3cfa0ba1e
Radu Maier <ariademur@webserv.co>
2020-07-31 19:14:14 +0200

    Do not ignore "none" for catno.

--- src/WebServCo/DiscogsApi/Parsers/Collection/CatNo.php
+++ src/WebServCo/DiscogsApi/Parsers/Collection/CatNo.php
@@ -8,7 +8,7 @@ final class CatNo implements \WebServCo\DiscogsApi\Interfaces\ParserInterface
         $result = null;
         if (isset($data['entity_type']) && isset($data['name'])) {
             if (1 == $data['entity_type']) { // label
-            if (!empty($data['catno'])/* && 'none' != $data['catno']*/) {
+                if (!empty($data['catno'])) {
                     $result = $data['catno'];
                 }
             }
